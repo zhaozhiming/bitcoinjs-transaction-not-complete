f7c62eb581ddb7869fa7969ad2a68f574f1ec0fb
zhaozhiming <zhaozhiming003@gmail.com>
2018-07-04 10:36:11 +0800

    change var to const

--- bitcoin.js
+++ bitcoin.js
@@ -1,22 +1,22 @@
 const bitcoin = require('bitcoinjs-lib');
 
 const createSegwitTransaction = () => {
-  var testnet = bitcoin.networks.testnet;
-  var keyPair = bitcoin.ECPair.fromWIF(
+  const testnet = bitcoin.networks.testnet;
+  const keyPair = bitcoin.ECPair.fromWIF(
     'cPJb8Hm2FyoKe8imdeMFSa8MkyfZKAm35PrYJB4b4oJHudx7WqJU',
     testnet
   );
-  var pubKey = keyPair.getPublicKeyBuffer();
-  var pubKeyHash = bitcoin.crypto.hash160(pubKey);
+  const pubKey = keyPair.getPublicKeyBuffer();
+  const pubKeyHash = bitcoin.crypto.hash160(pubKey);
 
-  var redeemScript = bitcoin.script.witnessPubKeyHash.output.encode(pubKeyHash);
-  var redeemScriptHash = bitcoin.crypto.hash160(redeemScript);
+  const redeemScript = bitcoin.script.witnessPubKeyHash.output.encode(pubKeyHash);
+  const redeemScriptHash = bitcoin.crypto.hash160(redeemScript);
 
-  var scriptPubKey = bitcoin.script.scriptHash.output.encode(redeemScriptHash);
-  var address = bitcoin.address.fromOutputScript(scriptPubKey, testnet);
+  const scriptPubKey = bitcoin.script.scriptHash.output.encode(redeemScriptHash);
+  const address = bitcoin.address.fromOutputScript(scriptPubKey, testnet);
   console.log({ address });
 
-  var txb = new bitcoin.TransactionBuilder(testnet);
+  const txb = new bitcoin.TransactionBuilder(testnet);
   txb.addInput(
     'bc11768a23ba472ae4715dc04ff695770c8d13a0cc57d6d1d67734a383425e44',
     0
